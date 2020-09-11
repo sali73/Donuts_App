7937b9a6e7cdf26eb63fd5b396cd87c6102267d0
sali73 <sally.elgendy@hotmail.com>
2020-09-10 21:08:33 -0400

    create base.html file to extand it to other files

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , redirect
 from .models import Product
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from .forms import NewUserForm
@@ -29,29 +29,39 @@ def add_donuts_form_submission(request):
     product = Product(title=title,price=price,sale_price=sale_price,image=image,slug=slug,description=description)
     product.save()
     return render(request,"product/Add_Donuts.html")
-#
-# def register(request):
-#     if request.method == 'POST':
-#         form = NewUserForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             username = form.cleaned_data.get("username")
-#             messages.success(request, f"New Account Created: {username}")
-#             login(request, user)
-#             messages.info(request, f"You Are Now Logged In As: {username}")
-#             return redirect("api:home")
-#         else:
-#             for msg in form.error_messages:
-#                 messages.error(request, f"{msg}:{form.error_messages[msg]}")
-#
-#     form = NewUserForm
-#     return render(request,
-#                   "product/register.html",
-#                   context={"form": form})
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {"form": form})
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"New Account Created: {username}")
+            login(request, user)
+            messages.info(request, f"You Are Now Logged In As: {username}")
+            return redirect("api:home")
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}:{form.error_messages[msg]}")
+
+    form = NewUserForm
+    return render(request,
+                  "users/register.html",
+                  context={"form": form})
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#                 username = form.cleaned_data.get("username")
+#                 messages.success(request, f"Account created for'{username} !")
+#                 login(request, username)
+#                 messages.info(request, f"You Are Now Logged In As: {username}")
+#                 return redirect("api:home")
+#             else:
+#                 for msg in form.error_messages:
+#                     messages.error(request, f"{msg}:{form.error_messages[msg]}")
+#     return render(request, 'users/register.html', {"form": form})
 
 def logout_request(request):
     logout(request)
@@ -73,10 +83,10 @@ def logout_request(request):
 #                 messages.error(request, "Invalid username or password.")
 #         else:
 #             messages.error(request, "Invalid username or password.")
-#
-#
 #     form = AuthenticationForm()
-#     return render(request,"product/login.html",{"form": form})
+#     return render(request,"users/login.html",{"form": form})
+
+
 def login_request(request):
     if request.method == 'POST':
         form = AuthenticationForm(request=request, data=request.POST)
@@ -94,5 +104,5 @@ def login_request(request):
             messages.error(request, "Invalid username or password.")
     form = AuthenticationForm()
     return render(request = request,
-                  template_name ="product/../templates/users/login.html",
+                  template_name ="users/login.html",
                   context={"form":form})
