08fc8f8a2f683b18855bd0496da13e7a142d09c7
sali73 <sally.elgendy@hotmail.com>
2020-09-14 22:06:35 -0400

    fix error in settings.py

--- donuts_app/settings.py
+++ donuts_app/settings.py
@@ -58,8 +58,7 @@ ROOT_URLCONF = 'donuts_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,7 +80,7 @@ WSGI_APPLICATION = 'donuts_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
