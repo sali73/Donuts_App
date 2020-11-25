b4bb38d874a7221ae69c2dd34b16f6a5521183b3
sali73 <sally.elgendy@hotmail.com>
2020-11-25 18:31:28 -0500

    change url for update cart

--- donuts_app/urls.py
+++ donuts_app/urls.py
@@ -18,7 +18,6 @@ from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from api import views
 
-from Donuts_App import api
 
 app_name = 'api'
 urlpatterns = [
@@ -35,7 +34,7 @@ urlpatterns = [
     path('<id>/update', views.update_view , name="update_view"),
     path('<id>/delete', views.delete_view , name= "delete_view"),
     path('cart/', views.view, name="cart"),
-    path('cart/(?P<slug>[^/]+)', api.views.update_cart, name="update_cart"),
+    path('cart/(?P<slug>[^/]+)', views.update_cart, name="update_cart"),
 
 ]
 
